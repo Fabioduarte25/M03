d7c6c95bcd9dad7a16ec38d96f58cdfd8cff2e6e
fabiocuevas7e5 <fabio.cuevas.7e5@itb.cat>
2021-10-21 12:26:24 +0200

    Initial Commit

---
+++ src/main/kotlin/cat/itb/fabiocuevas7e5/DAM/m03/uf1/data/seleccio/NiceIsValidNote1.kt
@@ -0,0 +1,11 @@
+package cat.itb.fabiocuevas7e5.DAM.m03.uf1.data.seleccio
+
+import java.util.*
+
+fun main(){
+    val scanner = Scanner(System.`in`)
+    val billete = scanner.nextInt()
+    val billets = listOf<Int>(5,10,20,50,100,200)
+    if(billete in billets) print("billet valid")
+    else print("billet invalid")
+}
---
+++ src/main/kotlin/cat/itb/fabiocuevas7e5/DAM/m03/uf1/data/seleccio/WhichPizzaShouldIBuy.kt
@@ -0,0 +1,20 @@
+package cat.itb.fabiocuevas7e5.DAM.m03.uf1.data.seleccio
+
+import java.util.*
+import kotlin.math.PI
+
+fun main (){
+    val scanner = Scanner(System.`in`)
+    val diametro = scanner.nextInt()
+    val largo = scanner.nextInt()
+    val ancho  = scanner.nextInt()
+    val radio = diametro/2
+    val rodona = PI*(radio*radio)
+    val rectangular = largo*ancho
+
+    if(rodona>rectangular)
+        print("Compra la rodona")
+    else print("Compra la rectangular")
+
+
+}
