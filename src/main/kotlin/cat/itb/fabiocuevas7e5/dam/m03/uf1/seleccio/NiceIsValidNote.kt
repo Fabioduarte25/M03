d7ce8d7f61345a85b37c23773250c41b45211c50
fabiocuevas7e5 <fabio.cuevas.7e5@itb.cat>
2021-10-22 13:01:18 +0200

    Initial Commit

--- src/main/kotlin/cat/itb/fabiocuevas7e5/dam/m03/uf1/seleccio/NiceIsValidNote.kt
+++ src/main/kotlin/cat/itb/fabiocuevas7e5/dam/m03/uf1/seleccio/NiceIsValidNote.kt
@@ -7,6 +7,5 @@ fun main(){
     val billete = scanner.nextInt()
     when (billete) {
         5, 10, 20, 50, 100, 500 -> print("bitllet vàlid")
-        else -> print("bitllet invàlid")
-    }
+        else -> print("bitllet invàlid")    }
 }
