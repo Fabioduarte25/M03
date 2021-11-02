5ff5f88fae0b8502d59acd1f76d32df0bd31117e
sjo <fabio.cuevas.7e5@itb.cat>
2021-11-02 13:06:15 +0100

    Initial commit

--- src/main/kotlin/cat/itb/fabiocuevas7e5/dam/m03/uf1/iterative/LetsCountBetween.kt
+++ src/main/kotlin/cat/itb/fabiocuevas7e5/dam/m03/uf1/iterative/LetsCountBetween.kt
@@ -6,5 +6,5 @@ fun main() {
     val sc = Scanner(System.`in`)
     val enter1 = sc.nextInt()
     val enter2 = sc.nextInt()
-    for (i in enter1+1..enter2-1) print(i)
+    for (i in enter1+1..enter2-1) {print(i)}
 }
