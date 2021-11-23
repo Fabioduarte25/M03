59524d87f1ac48f57294892d73a7dfb9a601e595
fabio.cuevas.7e5 <fabio.cuevas.7e5@itb.cat>
2021-11-23 12:44:17 +0100

    Initial commit

--- src/main/kotlin/cat/itb/fabiocuevas7e5/dam/m03/uf1/iterative/project/Mastermind.kt
+++ src/main/kotlin/cat/itb/fabiocuevas7e5/dam/m03/uf1/iterative/project/Mastermind.kt
@@ -7,10 +7,10 @@ data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 fun main() {
     val scanner = Scanner(System.`in`)
     var secret = ""
+
+//GAME MODE
     println("Vols jugar en mode 1vs1 (1) o solitari (2)?")
     val gameMode = scanner.nextInt()
-
-
     if (gameMode ==1){
         println("Introdueix la paraula secreta")
         secret = scanner.next()
@@ -20,28 +20,33 @@ fun main() {
             secret = secret + randomChar()
         }
         println("Paraula secreta generada!")
-        println(secret)
+        //println(secret)
     }
 
+//COMENÇA EL JOC
     println("Comença el joc!")
 
-
+//JOC
     var turns = 0
     while(turns < 12){
         turns += 1
         println("Introdueix una combinació")
         val guess = scanner.next()
+        if (guess.length !==4){
+            println("La teva predicció ha de tenir 4 lletres")
+        }
+        if (guess.length == 4){
         var evaluation = evaluateWord(secret, guess)
-        println(evaluation)
+        if (evaluation.rightPosition !==4 && turns < 12){
+        println("Posicions correctes: ${evaluation.rightPosition}, Posicions incorrectes: ${evaluation.wrongPosition}")}
         if (evaluation.rightPosition == 4){
             println("Enhorabona! has guanyat")
         break}
         else if(evaluation.rightPosition !== 4 && turns == 12){
             println("Fi del joc. Has perdut!")
-            break
-        }
+            break}
     }
-
+    }
 }
 
 fun randomChar():Char{
