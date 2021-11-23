3e9b1a91e917c890bee027e507e691ad6b59a25b
fabio.cuevas.7e5 <fabio.cuevas.7e5@itb.cat>
2021-11-23 12:00:13 +0100

    Initial commit

--- src/main/kotlin/cat/itb/fabiocuevas7e5/dam/m03/uf1/iterative/HowManyLines.kt
+++
@@ -1,7 +0,0 @@
-package cat.itb.fabiocuevas7e5.dam.m03.uf1.iterative
-
-import java.util.*
-
-fun main(){
-
-}
--- src/main/kotlin/cat/itb/fabiocuevas7e5/dam/m03/uf1/iterative/project/Mastermind.kt
+++ src/main/kotlin/cat/itb/fabiocuevas7e5/dam/m03/uf1/iterative/project/Mastermind.kt
@@ -2,14 +2,46 @@ package cat.itb.fabiocuevas7e5.dam.m03.uf1.iterative.project
 
 import java.util.*
 
+
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 fun main() {
-    // Use only this scanner
     val scanner = Scanner(System.`in`)
-    // Your code
+    var secret = ""
+    println("Vols jugar en mode 1vs1 (1) o solitari (2)?")
+    val gameMode = scanner.nextInt()
 
-    //use this function
-    var evaluation = evaluateWord("ABCD", "AEEE")
+
+    if (gameMode ==1){
+        println("Introdueix la paraula secreta")
+        secret = scanner.next()
+    }
+    if (gameMode == 2) {
+        repeat(4){
+            secret = secret + randomChar()
+        }
+        println("Paraula secreta generada!")
+        println(secret)
+    }
+
+    println("Comença el joc!")
+
+
+    var turns = 0
+    while(turns < 12){
+        turns += 1
+        println("Introdueix una combinació")
+        val guess = scanner.next()
+        var evaluation = evaluateWord(secret, guess)
+        println(evaluation)
+        if (evaluation.rightPosition == 4){
+            println("Enhorabona! has guanyat")
+        break}
+        else if(evaluation.rightPosition !== 4 && turns == 12){
+            println("Fi del joc. Has perdut!")
+            break
+        }
+    }
+
 }
 
 fun randomChar():Char{
@@ -18,6 +50,13 @@ fun randomChar():Char{
 }
 
 fun evaluateWord(secret: String, guess: String): Evaluation {
-    //Calculate your right and wrong positions and change it as you need
-    return Evaluation(0,0)
-}
+        var rightPosition = 0
+        var wrongPosition = 0
+        for (i in 0 until 4){
+            if (guess[i] == secret[i])
+                rightPosition += 1
+            else if(guess[i] in secret)
+                wrongPosition += 1
+        }
+        return Evaluation(rightPosition, wrongPosition )
+    }
